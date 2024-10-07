b49d54850f9d8d7c15baaf781e894a1efede04e8
shangcom <dndbs91@gmail.com>
2024-10-07 14:42:41 +0900

    스프링 MVC 2편
    섹션7 로그인 처리2 - 필터, 인터셉터
    
    서블릿 필터 - 인증 체크
    
    완료

--- src/main/java/hello/login/web/login/LoginController.java
+++ src/main/java/hello/login/web/login/LoginController.java
@@ -12,6 +12,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
@@ -80,7 +81,7 @@ public class LoginController {
      * request로 session 생성.
      * request.getSession() : 세션이 있으면 세션 반환 없으면 신규 세션 생성 및 반환
      */
-    @PostMapping("/login")
+//    @PostMapping("/login")
     public String loginV3(@Validated @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult, HttpServletRequest request) {
 
         if (bindingResult.hasErrors()) {
@@ -101,6 +102,30 @@ public class LoginController {
 
         return "redirect:/";
     }
+
+
+    @PostMapping("/login")
+    public String loginV4(@Validated @ModelAttribute("loginForm") LoginForm form, BindingResult bindingResult,
+                          @RequestParam(defaultValue = "/") String redirectURL,
+                          HttpServletRequest request) {
+
+        if (bindingResult.hasErrors()) {
+            return "/login/loginForm";
+        }
+
+        Member loginMember = loginService.login(form.getLoginId(), form.getPassword());
+
+        if (loginMember == null) {
+            bindingResult.reject("loginFail", "아이디 또는 비밀번호가 맞지 않습니다.");
+            return "login/loginForm";
+        }
+
+        HttpSession session = request.getSession();
+        session.setAttribute(SessionConst.LOGIN_MEMBER, loginMember);
+
+        return "redirect:" + redirectURL;
+    }
+
 
 //    @PostMapping("/logout")
     public String logout(HttpServletResponse response) {
